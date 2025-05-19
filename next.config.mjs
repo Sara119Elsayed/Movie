ae890b312110c7a3cbb00aaf186165490afc8caa
SaraElsayed <esara7772@gmail.com>
2025-05-19 23:48:15 +0300

    next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,33 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.jsdelivr.net',
+        port: '',
+        pathname: '/gh/faker-js/assets-person-portrait/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        port: '',
+        pathname: '/t/p/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lumiere-a.akamaihd.net',
+        port: '',
+        pathname: '/**', // Or be more specific if needed
+      },
+    ],
+  },
+};
+export default nextConfig;
